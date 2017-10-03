8644a3ef335c6d14918cdd6e2f57b81fa8d6bb54
Cleiton Rezende Ferreira <cleitonrferreira@gmail.com>
2017-10-02 21:34:50 -0300

    fix param type

--- fibcalc-api/src/main/java/br/edu/univas/si8/ta/fibcalc/api/FibService.java
+++ fibcalc-api/src/main/java/br/edu/univas/si8/ta/fibcalc/api/FibService.java
@@ -4,7 +4,6 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 @Path("/fibcalc")
@@ -13,12 +12,6 @@ public interface FibService {
 	@GET
 	@Path("/fib/{num}")
 	@Produces(MediaType.APPLICATION_JSON)
-	String fib(@QueryParam("num") String num);
-	
-	
-	@GET
-	@Path("/say/{name}")
-	@Produces(MediaType.TEXT_HTML)
-	String sayHello(@PathParam("name") String name);
+	Result fib(@PathParam("num") int num);
 	
 }
--- fibcalc-core/src/main/java/br/edu/univas/si8/ta/fibcalc/core/FibonacciImpl.java
+++ fibcalc-core/src/main/java/br/edu/univas/si8/ta/fibcalc/core/FibonacciImpl.java
@@ -7,12 +7,9 @@ import br.edu.univas.si8.ta.fibcalc.lib.CalculadoraFibonacci;
 public class FibonacciImpl implements FibService{
 
 	@Override
-	public String fib(int num) {
+	public Result fib(int num) {
 		
-		return String.format("<h1>Hello %s!!!</h1>", num);
-		
-		//int resultado = CalculadoraFibonacci.fibonacci(num);
-		//return new Result().withNumber(num).withFibonacci(resultado);
-		
+		int resultado = CalculadoraFibonacci.fibonacci(num);
+		return new Result().withNumber(num).withFibonacci(resultado);
 	}
 }
